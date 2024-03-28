811240eaac6977397514a10f86b175221484e7e2
firatstory.eth <talefirat@gmail.com>
2024-03-28 15:14:59 +0100

    refactor: :recycle: handle first test case

--- test/access-control-1/TestAC1.t.sol
+++ test/access-control-1/TestAC1.t.sol
@@ -1,8 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-import "forge-std/console.sol";
 
 interface IProtocolVault {
     function withdrawETH() external;
@@ -12,70 +11,61 @@ interface IProtocolVault {
     function _sendETH(address to) external;
 }
 
-/**
- * @dev run "forge test --match-contract AC1"
- */
-contract TestAC1 is Test {
-    IProtocolVault vault;
-
-    uint128 public constant DEFAULT_BAL = 100 ether;
-
-    uint128 public constant USER_DEPOSIT = 10 ether;
-
-    uint256 init_attacker_bal;
-    uint256 init_vault_bal;
-
-    address deployer;
-    address user1;
-    address user2;
-    address user3;
-    address attacker;
+contract AccessControlExercise1Test is Test {
+    IProtocolVault private protocolVault;
+    address private deployer;
+    address private user1;
+    address private user2;
+    address private user3;
+    address private attacker;
+    uint256 private constant USER_DEPOSIT = 10 ether;
+    uint256 private attackerInitialETHBalance;
 
     function setUp() public {
-        deployer = address(1);
-        user1 = address(2);
-        user2 = address(3);
-        user3 = address(4);
-        attacker = address(5);
-        vm.deal(user1, DEFAULT_BAL);
-        vm.deal(user2, DEFAULT_BAL);
-        vm.deal(user3, DEFAULT_BAL);
-        vm.deal(attacker, DEFAULT_BAL);
+        // SETUP EXERCISE - DON'T CHANGE ANYTHING HERE
+        deployer = address(this); // In Foundry, the deploying contract is the test contract itself
+        user1 = makeAddr("user1");
+        user2 = makeAddr("user2");
+        user3 = makeAddr("user3");
+        attacker = makeAddr("attacker");
+
+        attackerInitialETHBalance = attacker.balance;
 
         //Due to incompatible solidity versions (0.4 v/s 0.8), we are directly deploying the
         //compiled bytecode on blockchain on behalf of deployer using "deployCode"
         vm.startPrank(deployer);
         address _attack = deployCode("ProtocolVault.sol:ProtocolVault");
-        vault = IProtocolVault(_attack);
+        protocolVault = IProtocolVault(_attack);
         vm.stopPrank();
 
-        //Users sending funds to the vault
         vm.prank(user1);
-        (bool success1,) = address(vault).call{value: USER_DEPOSIT}("");
-        require(success1, "Transfer failed");
+        payable(address(protocolVault)).transfer(USER_DEPOSIT);
 
         vm.prank(user2);
-        (bool success2,) = address(vault).call{value: USER_DEPOSIT}("");
-        require(success2, "Transfer failed");
+        payable(address(protocolVault)).transfer(USER_DEPOSIT);
 
         vm.prank(user3);
-        (bool success3,) = address(vault).call{value: USER_DEPOSIT}("");
-        require(success3, "Transfer failed");
+        payable(address(protocolVault)).transfer(USER_DEPOSIT);
 
-        init_vault_bal = address(vault).balance;
-        init_attacker_bal = address(attacker).balance;
+        uint256 currentBalance = address(protocolVault).balance;
+        assertEq(currentBalance, USER_DEPOSIT * 3);
 
-        // Protocol Vault has 30 ETH, attacker has DEFAULT_BALANCE
-        assertEq(init_vault_bal, USER_DEPOSIT * 3);
-        assertEq(init_attacker_bal, DEFAULT_BAL);
+        vm.prank(attacker);
+        vm.expectRevert();
+        protocolVault.withdrawETH();
     }
 
-    function test_Attack() public {
-        vm.prank(attacker);
-        vault._sendETH(attacker);
+    function testExploit() public {
+        // CODE YOUR SOLUTION HERE
+    }
+
+    function tearDown() public view {
+        // SUCCESS CONDITIONS
 
         // Protocol Vault is empty and attacker has ~30+ ETH
-        assertEq(address(vault).balance, 0);
-        assertEq(address(attacker).balance, init_attacker_bal + (USER_DEPOSIT * 3));
+        assertEq(address(protocolVault).balance, 0);
+
+        // It's difficult to account for exact transaction costs, hence the approximate check
+        assertTrue(attacker.balance >= attackerInitialETHBalance + (USER_DEPOSIT * 3) - 0.2 ether);
     }
 }
