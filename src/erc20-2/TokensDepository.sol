b8023e84e4da61f50a466db0b3e2bbb8254b0d70
firatstory.eth <talefirat@gmail.com>
2024-04-04 12:13:20 +0200

    feat: :sparkles: add TokensDepository.sol

--- src/erc20-2/TokensDepository.sol
+++ src/erc20-2/TokensDepository.sol
@@ -10,4 +10,38 @@ import {rToken} from "./rToken.sol";
  * @title TokensDepository
  * @author FiratStory (firatstory.eth)
  */
-contract TokensDepository {}
+contract TokensDepository {
+    mapping(address tokenAddress => rToken rToken) public rTokens;
+
+    event Deposit(address indexed tokenAddress, address indexed user, uint256 amount);
+    event Withdraw(address indexed tokenAddress, address indexed user, uint256 amount);
+
+    modifier validToken(address tokenAddress) {
+        require(rTokens[tokenAddress] != rToken(address(0)), "Invalid token address");
+        _;
+    }
+
+    constructor(address _aave, address _uni, address _weth) {
+        rTokens[_aave] = new rToken(_aave, "AAVE RToken", "rAAVE");
+        rTokens[_uni] = new rToken(_uni, "UNI RToken", "rUNI");
+        rTokens[_weth] = new rToken(_weth, "WETH RToken", "rWETH");
+    }
+
+    function deposit(address tokenAddress, uint256 amount) external validToken(tokenAddress) {
+        bool success = IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
+        require(success, "Transfer failed");
+
+        rTokens[tokenAddress].mint(msg.sender, amount);
+
+        emit Deposit(tokenAddress, msg.sender, amount);
+    }
+
+    function withdraw(address tokenAddress, uint256 amount) external validToken(tokenAddress) {
+        bool success = IERC20(tokenAddress).transfer(msg.sender, amount);
+        require(success, "Transfer failed");
+
+        rTokens[tokenAddress].burn(msg.sender, amount);
+
+        emit Withdraw(tokenAddress, msg.sender, amount);
+    }
+}
